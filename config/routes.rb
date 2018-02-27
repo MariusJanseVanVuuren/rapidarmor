b59d236b8b7ffb23b7445b7df90b8d7d39ad02ba
mariusjvn-glucode <marius@glucode.com>
2018-02-27 15:01:01 +0200

    remove routing

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   resources :companies do
     collection { post :import }
   end
-  root to: "companies#index"
+  # root to: "companies#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
