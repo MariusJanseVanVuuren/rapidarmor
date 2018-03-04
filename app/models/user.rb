043d5a347051b4898754713d67a5b8c9113bbc37
mariusjvn-glucode <marius@glucode.com>
2018-03-04 18:13:57 +0200

    Added login redirect for non loged in user

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,14 +10,4 @@ class User < ApplicationRecord
       user = User.create params
     end
   end
-
-  def self.authenticate(email, submitted_password)
-    user = find_by_email(email)
-    return nil  if user.nil?
-    return user if user.password_digest == submitted_password
-  end
-
-  def authenticate(submitted_password)
-    self.password_digest == submitted_password
-  end
 end
